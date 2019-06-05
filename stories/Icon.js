c1369b940813f4653f151009b1ed92992f589232
Edyta SKIBINSKA <eskibinska@centreon.com>
2019-06-05 16:53:27 +0200

    enh(UI) adding MUI icons to storybook in SVG format

--- stories/Icon.js
+++ stories/Icon.js
@@ -9,8 +9,51 @@ import {
   IconNumber,
   IconRound,
   IconToggleSubmenu,
-  IconLegend
+  IconLegend,
+  DeleteIcon,
+  FlipToFrontIcon,
+  EditIcon,
+  AccessTimeIcon,
+  DoneIcon,
+  ReportProblemIcon,
+  SearchIcon,
+  PlayCircleOutlineIcon,
+  StopOutlinedIcon,
+  UpdateOutlinedIcon,
+  KeyboardArrowRightIcon,
+  KeyboardArrowLeftIcon,
+  DescriptionOutlinedIcon,
+  UnarchiveOutlinedIcon,
+  CloudOffOutlinedIcon,
+  CloudUploadOutlinedIcon
 } from "../src";
+
+
+storiesOf("Icon", module).add(
+  "Icons - MUI",
+() => {
+  return (
+  <React.Fragment>
+    <FlipToFrontIcon/>
+    <EditIcon />
+    <DeleteIcon />
+    <AccessTimeIcon/>
+    <DoneIcon/>
+    <ReportProblemIcon/>
+    <SearchIcon/>
+    <PlayCircleOutlineIcon/>
+    <StopOutlinedIcon/>
+    <UpdateOutlinedIcon/>
+    <KeyboardArrowRightIcon/>
+    <KeyboardArrowLeftIcon/>
+    <DescriptionOutlinedIcon/>
+    <UnarchiveOutlinedIcon/>
+    <CloudOffOutlinedIcon/>
+    <CloudUploadOutlinedIcon/>
+    <p>MUI Icon base : https://material.io/tools/icons/?style=baseline</p>
+  </React.Fragment>)},
+  { notes: "MUI Icon base : https://material.io/tools/icons/?style=baseline" }
+);
 
 storiesOf("Icon", module).add(
   "Icon - action",
