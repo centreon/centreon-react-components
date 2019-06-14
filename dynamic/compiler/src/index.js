2b75eb00ebeeb6cbee05bff99636f0c395caceb2
Bruno d'Auria <bdauria@centreon.com>
2019-06-14 10:58:39 +0200

    add dynamic/compiler/src/index.js to .eslintignore and replace back const with vars.

--- dynamic/compiler/src/index.js
+++ dynamic/compiler/src/index.js
@@ -2,8 +2,8 @@ import DynamicComponentSource from './example2'; // TO:DO Replace with process.e
 
 if (window.parent) {
   window.parent[process.env.COMPONENT_NAME] = DynamicComponentSource;
-  const elem = window.parent.document;
-  const event = elem.createEvent('Event');
+  var elem = window.parent.document;
+  var event = elem.createEvent('Event');
   event.initEvent(`component${process.env.COMPONENT_NAME}Loaded`, false, true);
   elem.dispatchEvent(event);
 }
