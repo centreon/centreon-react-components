541dbf9d809d29ee4aa93d5353041e50e04be1fc
Bruno d'Auria <bdauria@centreon.com>
2019-06-21 14:07:14 +0200

    Fix stories not returning correct React elements.

--- stories/InputField.js
+++ stories/InputField.js
@@ -1,3 +1,6 @@
+/* eslint-disable react/jsx-indent */
+/* eslint-disable react/jsx-wrap-multilines */
+/* eslint-disable prettier/prettier */
 /* eslint-disable no-sequences */
 /* eslint-disable react/jsx-filename-extension */
 /* eslint-disable import/no-extraneous-dependencies */
@@ -16,17 +19,14 @@ import {
 
 storiesOf('Input Field', module).add(
   'Input Field - with title',
-  () => (
-    (
-      <InputField
-        type="text"
-        label="Input field with title"
-        name="test"
-        inputSize="small"
-      />
-    ),
+  () =>
+    <InputField
+      type="text"
+      label="Input field with title"
+      name="test"
+      inputSize="small"
+    />,
     { notes: 'A very simple component' }
-  ),
 );
 
 storiesOf('Input Field', module).add(
@@ -38,42 +38,35 @@ storiesOf('Input Field', module).add(
 storiesOf('Input Field', module).add(
   'Input Field - without title',
   () => <InputField type="text" name="test" inputSize="small" />,
-  { notes: 'A very simple component' },
+  { notes: 'A very simple component' }
 );
 
 storiesOf('Input Field', module).add(
   'Input Field error - without title',
-  () => (
-    (
+  () =>
       <InputField
         type="text"
         name="test"
         error="The field is mandatory"
         inputSize="small"
-      />
-    ),
+      />,
     { notes: 'A very simple component' }
-  ),
 );
 
 storiesOf('Input Field', module).add(
   'Input Field - select',
-  () => (
-    (
+  () =>
       <InputFieldSelect
         customClass={classnames(styles['select-option-custom'])}
-      />
-    ),
+      />,
     { notes: 'A very simple component' }
-  ),
 );
 
 storiesOf('Input Field', module).add(
   'Input Field - textarea',
-  () => (
+  () =>
     <InputFieldTextarea textareaType="small" label="Textarea field label" />,
     { notes: 'A very simple component' }
-  ),
 );
 
 storiesOf('Input Field', module).add(
