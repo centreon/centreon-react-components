55b5261c2420d9caeb66d65003d7130030996337
Bruno d'Auria <bdauria@centreon.com>
2019-07-12 15:09:00 +0200

    Add jest-emotion serializer and react-test-renderer.

--- setupTests.js
+++ setupTests.js
@@ -1,5 +1,8 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import emotionSerializer from 'jest-emotion'
+
+expect.addSnapshotSerializer(emotionSerializer);
 
 Enzyme.configure({ adapter: new Adapter() });
