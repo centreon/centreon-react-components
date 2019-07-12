2063b769620787790a87b97832e6896211882657
Bruno d'Auria <bdauria@centreon.com>
2019-07-12 15:30:21 +0200

    Fix ESLint errors.

--- setupTests.js
+++ setupTests.js
@@ -1,7 +1,9 @@
 /* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable no-undef */
+
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import emotionSerializer from 'jest-emotion'
+import emotionSerializer from 'jest-emotion';
 
 expect.addSnapshotSerializer(emotionSerializer);
 
