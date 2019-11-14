8054e1deb220806cf16678f842a19592d1a7575c
Kevin Duret <kduret@centreon.com>
2019-11-14 13:42:46 +0100

    fix coding style in unit tests

--- src/Wizard/index.test.jsx
+++ src/Wizard/index.test.jsx
@@ -2,6 +2,22 @@ import React from 'react';
 import { act, render, fireEvent } from '@testing-library/react';
 import Wizard, { Page } from '.';
 
+function renderWizard() {
+  return render(
+    <Wizard open>
+      <Page label="step label 1">
+        <div>Step 1</div>
+      </Page>
+      <Page label="step label 2">
+        <div>Step 2</div>
+      </Page>
+      <Page label="step label 3">
+        <div>Step 3</div>
+      </Page>
+    </Wizard>,
+  );
+}
+
 describe('Wizard', () => {
   it('displays step labels', () => {
     const { getByText } = renderWizard();
@@ -26,20 +42,4 @@ describe('Wizard', () => {
 
     expect(getByText('Step 1')).toBeInTheDocument();
   });
-
-  function renderWizard() {
-    return render(
-      <Wizard open>
-        <Page label="step label 1">
-          <div>Step 1</div>
-        </Page>
-        <Page label="step label 2">
-          <div>Step 2</div>
-        </Page>
-        <Page label="step label 3">
-          <div>Step 3</div>
-        </Page>
-      </Wizard>,
-    );
-  }
 });
