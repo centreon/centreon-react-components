173a105387eaa36a7ac16b79651d4fed51097a37
Bruno d'Auria <bdauria@centreon.com>
2019-12-22 19:50:34 +0100

    try new install script

--- install-chrome-headless.sh
+++ install-chrome-headless.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-curl https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add - \
-    && sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list' \
-    && apt-get update \
-    && apt-get install -y google-chrome-stable build-essential jq git openssh-server \
-    && apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+sudo apt-get update
+sudo apt-get install -y libappindicator1 fonts-liberation
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome*.de
