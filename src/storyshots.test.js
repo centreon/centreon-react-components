3a40c2e45848bc05dded5686f35af1beb452ccab
Bruno d'Auria <bdauria@centreon.com>
2019-12-22 21:37:31 +0100

    udpate snapshots and add delay + thresholds

--- src/storyshots.test.js
+++ src/storyshots.test.js
@@ -1,7 +1,23 @@
 import initStoryshots from '@storybook/addon-storyshots';
 import { imageSnapshot } from '@storybook/addon-storyshots-puppeteer';
 
+const getMatchOptions = () => {
+  return {
+    failureThreshold: 0.2,
+    failureThresholdType: 'percent',
+  };
+};
+const beforeScreenshot = () => {
+  return new Promise((resolve) =>
+    setTimeout(() => {
+      resolve();
+    }, 600),
+  );
+};
+
 initStoryshots({
   suite: 'Image StoryShots',
   test: imageSnapshot({ storybookUrl: `file://${__dirname}/../.out` }),
+  getMatchOptions,
+  beforeScreenshot,
 });
