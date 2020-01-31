fcadb8b1976c92e09567e072f990803c47cef7be
Bruno d'Auria <bdauria@centreon.com>
2020-01-31 18:31:47 +0100

    Update configuration to support TypeScript

---
+++ src/Icon/IconCollapse/index.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import IconCollapse from '.';
+
+export default { title: 'Icon/Collapse' };
+
+export const normal = (): JSX.Element => <IconCollapse />;
+
+export const toggled = (): JSX.Element => <IconCollapse toggled />;
--- src/Icon/IconCollapse/index.tsx
+++ src/Icon/IconCollapse/index.tsx
@@ -5,13 +5,13 @@ import { KeyboardArrowDown, KeyboardArrowUp } from '@material-ui/icons';
 import MaterialIcon from '../MaterialIcon';
 
 interface Props {
-  toggled: boolean;
+  toggled?: boolean;
 }
 
 const IconArrowUpDown = ({ toggled = false, ...props }: Props): JSX.Element => (
-    <MaterialIcon {...props}>
-      {toggled ? <KeyboardArrowDown /> : <KeyboardArrowUp />}
-    </MaterialIcon>
-  );
+  <MaterialIcon {...props}>
+    {toggled ? <KeyboardArrowDown /> : <KeyboardArrowUp />}
+  </MaterialIcon>
+);
 
 export default IconArrowUpDown;
