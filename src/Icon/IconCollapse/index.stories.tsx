c065e8bc4ff76b4c7b420075a69fdb0821ae6fec
Bruno d'Auria <bdauria@centreon.com>
2020-02-05 18:10:22 +0100

    Remove IconCollapse Icon and start converting Listing to TypeScript

--- src/Icon/IconCollapse/index.stories.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import IconCollapse from '.';
-
-export default { title: 'Icon/Collapse' };
-
-export const normal = (): JSX.Element => <IconCollapse />;
-
-export const toggled = (): JSX.Element => <IconCollapse toggled />;
