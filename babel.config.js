8677197455e1e049d3ef0187c464cd28f7bbe554
Bruno d'Auria <bdauria@centreon.com>
2020-02-06 10:33:54 +0100

    Update babel-config

--- babel.config.js
+++ babel.config.js
@@ -1,35 +1,35 @@
-module.exports = {
+const merge = require('babel-merge');
+
+const configuration = {
   presets: [
-    ['@babel/preset-react'],
+    '@babel/preset-typescript',
+    '@babel/preset-react',
     [
       '@babel/preset-env',
       {
-        targets: {
-          esmodules: false,
-        },
+        modules: false,
       },
     ],
   ],
-  plugins: [
-    '@babel/plugin-syntax-dynamic-import',
-    '@babel/plugin-transform-arrow-functions',
-    '@babel/plugin-transform-destructuring',
-    '@babel/plugin-transform-function-name',
-    '@babel/plugin-transform-parameters',
-    '@babel/plugin-proposal-class-properties',
-    '@babel/plugin-transform-classes',
-    '@babel/plugin-transform-shorthand-properties',
-    '@babel/plugin-transform-regenerator',
-    [
-      '@babel/plugin-transform-runtime',
-      {
-        regenerator: true,
-      },
-    ],
-  ],
+  plugins: ['@babel/proposal-class-properties'],
+};
+
+module.exports = {
   env: {
-    test: {
+    production: configuration,
+    development: configuration,
+    test: merge(configuration, {
+      presets: [
+        [
+          '@babel/preset-env',
+          {
+            targets: {
+              node: 'current',
+            },
+          },
+        ],
+      ],
       plugins: ['require-context-hook'],
-    },
+    }),
   },
 };
