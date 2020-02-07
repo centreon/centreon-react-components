f9cd6395704f1ee5fbe479da49fc04ad43089fa2
Bruno d'Auria <bdauria@centreon.com>
2020-02-07 10:53:01 +0100

    Update webpack config

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,55 +1,46 @@
-const path = require('path');
+const merge = require('lodash/merge');
+const webpackMerge = require('webpack-merge');
+const baseConfig = require('@centreon/frontend-core').webpackTypeScript;
 
 const storiesPath = '../src/**/*.stories';
 
 module.exports = {
-  stories: [`${storiesPath}.jsx`, `${storiesPath}.tsx`],
-  addons: [],
-  webpackFinal: (config) => ({
-    ...config,
-    resolve: {
-      extensions: ['.js', '.jsx', '.ts', '.tsx'],
-    },
-    module: {
-      ...config.module,
-      rules: [
-        {
-          test: /\.jsx?$/,
-          include: path.resolve(__dirname, '..'),
-          use: [{ loader: 'babel-loader' }],
-        },
-        {
-          test: /\.tsx?$/,
-          include: path.resolve(__dirname, '..'),
-          use: ['babel-loader', 'awesome-typescript-loader'],
-        },
-        {
-          test: /\.s?[ac]ss$/i,
-          use: [
-            'style-loader',
-            {
-              loader: 'css-loader',
-              options: {
-                modules: {
-                  localIdentName: '[local]__[hash:base64:5]',
-                },
-              },
-            },
-            'sass-loader',
-          ],
-        },
-        {
-          test: /\.(?:woff|woff2|eot|ttf|otf)$/,
-          loader: 'file-loader',
-        },
-        {
-          test: /\.(?:png|jpg|svg)$/,
-          loader: 'url-loader',
-          query: {
-            limit: 10000,
-          },
-        },
-      ],
-    },
-  }),
+	stories: [`${storiesPath}.jsx`, `${storiesPath}.tsx`],
+	addons: [],
+	webpackFinal: (config) =>
+		merge(
+			config,
+			webpackMerge(baseConfig, {
+				module: {
+					rules: [
+						{
+							test: /\.s?[ac]ss$/i,
+							use: [
+								'style-loader',
+								{
+									loader: 'css-loader',
+									options: {
+										modules: {
+											localIdentName: '[local]__[hash:base64:5]',
+										},
+									},
+								},
+								'sass-loader',
+							],
+						},
+						{
+							test: /\.(?:woff|woff2|eot|ttf|otf)$/,
+							loader: 'file-loader',
+						},
+						{
+							test: /\.(?:png|jpg|svg)$/,
+							loader: 'url-loader',
+							query: {
+								limit: 10000,
+							},
+						},
+					],
+				},
+			})
+		),
 };
