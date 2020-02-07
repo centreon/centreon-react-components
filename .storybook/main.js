091263cb77c55ef21a65c0c4e099f9dd3a1a0757
Bruno d'Auria <bdauria@centreon.com>
2020-02-07 11:16:47 +0100

    fix indent

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,42 +5,42 @@ const baseConfig = require('@centreon/frontend-core').webpackTypeScript;
 const storiesPath = '../src/**/*.stories';
 
 module.exports = {
-	stories: [`${storiesPath}.jsx`, `${storiesPath}.tsx`],
-	addons: [],
-	webpackFinal: (config) =>
-		merge(
-			config,
-			webpackMerge(baseConfig, {
-				module: {
-					rules: [
-						{
-							test: /\.s?[ac]ss$/i,
-							use: [
-								'style-loader',
-								{
-									loader: 'css-loader',
-									options: {
-										modules: {
-											localIdentName: '[local]__[hash:base64:5]',
-										},
-									},
-								},
-								'sass-loader',
-							],
-						},
-						{
-							test: /\.(?:woff|woff2|eot|ttf|otf)$/,
-							loader: 'file-loader',
-						},
-						{
-							test: /\.(?:png|jpg|svg)$/,
-							loader: 'url-loader',
-							query: {
-								limit: 10000,
-							},
-						},
-					],
-				},
-			})
-		),
+  stories: [`${storiesPath}.jsx`, `${storiesPath}.tsx`],
+  addons: [],
+  webpackFinal: (config) =>
+    merge(
+      config,
+      webpackMerge(baseConfig, {
+        module: {
+          rules: [
+            {
+              test: /\.s?[ac]ss$/i,
+              use: [
+                'style-loader',
+                {
+                  loader: 'css-loader',
+                  options: {
+                    modules: {
+                      localIdentName: '[local]__[hash:base64:5]',
+                    },
+                  },
+                },
+                'sass-loader',
+              ],
+            },
+            {
+              test: /\.(?:woff|woff2|eot|ttf|otf)$/,
+              loader: 'file-loader',
+            },
+            {
+              test: /\.(?:png|jpg|svg)$/,
+              loader: 'url-loader',
+              query: {
+                limit: 10000,
+              },
+            },
+          ],
+        },
+      })
+    ),
 };
