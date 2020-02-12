a3bf0202f2711e249a632e5db8818c86acb10277
Bruno d'Auria <bdauria@centreon.com>
2020-02-12 14:08:35 +0100

    Fix indent

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,16 +5,16 @@ const baseConfig = require('@centreon/frontend-core/webpack/base/typescript');
 const storiesPath = '../src/**/*.stories';
 
 module.exports = {
-	stories: ['../src/**/*.stories.(jsx|tsx)'],
-	addons: [],
-	webpackFinal: config => ({
+  stories: ['../src/**/*.stories.(jsx|tsx)'],
+  addons: [],
+  webpackFinal: config => ({
     ...config,
     resolve: {
       ...config.resolve,
       extensions: ['.js', '.jsx', '.ts', '.tsx'],
     },
-		module: { ...config.module,
-			rules: [
+    module: { ...config.module,
+      rules: [
         {
           test: /\.jsx?$/,
           exclude: /node_modules/,
@@ -25,33 +25,33 @@ module.exports = {
           exclude: /node_modules/,
           use: ['babel-loader', 'awesome-typescript-loader'],
         },
-				{
-					test: /\.s?[ac]ss$/i,
-					use: [
-						'style-loader',
-						{
-							loader: 'css-loader',
-							options: {
-								modules: {
-									localIdentName: '[local]__[hash:base64:5]',
-								},
-							},
-						},
-						'sass-loader',
-					],
-				},
-				{
-					test: /\.(?:woff|woff2|eot|ttf|otf)$/,
-					loader: 'file-loader',
-				},
-				{
-					test: /\.(?:png|jpg|svg)$/,
-					loader: 'url-loader',
-					query: {
-						limit: 10000,
-					},
-				},
-			],
-		},
-	}),
+        {
+          test: /\.s?[ac]ss$/i,
+          use: [
+            'style-loader',
+            {
+              loader: 'css-loader',
+              options: {
+                modules: {
+                  localIdentName: '[local]__[hash:base64:5]',
+                },
+              },
+            },
+            'sass-loader',
+          ],
+        },
+        {
+          test: /\.(?:woff|woff2|eot|ttf|otf)$/,
+          loader: 'file-loader',
+        },
+        {
+          test: /\.(?:png|jpg|svg)$/,
+          loader: 'url-loader',
+          query: {
+            limit: 10000,
+          },
+        },
+      ],
+    },
+  }),
 };
