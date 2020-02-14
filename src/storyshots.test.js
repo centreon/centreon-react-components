fa2bc75f68ce3c268d169c0f0ed2e01e35d98239
tdarneix <tdarneix@centreon.com>
2020-02-14 13:25:52 +0100

    Implement logic that allow to run storyshots on specific components

--- src/storyshots.test.js
+++ src/storyshots.test.js
@@ -15,11 +15,21 @@ const beforeScreenshot = () => {
   );
 };
 
+const idxStoryArg = process.argv.indexOf('-story');
+let regexp = null;
+if (idxStoryArg !== -1) {
+  const storyComponents = process.argv.slice(idxStoryArg + 1);
+  const componentsToTest = storyComponents.length > 0 ?
+    storyComponents[0].split(' ').join('|') : '';
+  regexp = componentsToTest ? new RegExp(`^(${componentsToTest})$`, "g") : null;
+}
+
 initStoryshots({
   suite: 'Image StoryShots',
+  storyKindRegex: regexp,
   test: imageSnapshot({
     storybookUrl: `file://${__dirname}/../.out`,
     getMatchOptions,
     beforeScreenshot,
-  }),
+  })
 });
