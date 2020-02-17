72d464148538c9b88bd92d1ec51e2d1abdd989c5
tdarneix <tdarneix@centreon.com>
2020-02-17 17:22:15 +0100

    Add story

--- src/InputField/InputFieldSelectCustom/index.stories.jsx
+++ src/InputField/InputFieldSelectCustom/index.stories.jsx
@@ -17,6 +17,20 @@ export const normal = () => (
   />
 );
 
+export const withPreselectedValue = () => (
+  <InputFieldSelect
+    options={[
+      { id: '1', name: '24x7', alias: 'Always' },
+      { id: '2', name: 'none', alias: 'Never' },
+      { id: '3', name: 'nonworkhours', alias: 'Non-Work Hours' },
+      { id: '4', name: 'workhours', alias: 'Work hours' },
+    ]}
+    active="active"
+    size="medium"
+    value={{ id: '2', name: 'none', alias: 'Never' }}
+  />
+);
+
 export const withError = () => (
   <InputFieldSelect error="The field is mandatory" size="medium" />
 );
