6e8f44546a0d2e161e46782eba74bb0f0488a35e
Bruno d'Auria <bdauria@centreon.com>
2020-02-19 11:06:54 +0100

    Fix Select

--- src/InputField/Select/index.tsx
+++ src/InputField/Select/index.tsx
@@ -1,5 +1,21 @@
 import React from 'react';
-import { Select, MenuItem, FormControl, InputLabel } from '@material-ui/core';
+
+import clsx from 'clsx';
+
+import {
+  Select,
+  MenuItem,
+  FormControl,
+  InputLabel,
+  makeStyles,
+  Theme,
+} from '@material-ui/core';
+
+const useStyles = makeStyles((theme: Theme) => ({
+  noLabelInput: {
+    padding: theme.spacing(1.5),
+  },
+}));
 
 interface SelectEntry {
   id: number;
@@ -10,7 +26,7 @@ interface Props {
   options: Array<SelectEntry>;
   onChange;
   selectedOptionId: number;
-  label: string;
+  label?: string;
 }
 
 const SelectField = ({
@@ -19,10 +35,19 @@ const SelectField = ({
   selectedOptionId,
   label,
 }: Props): JSX.Element => {
+  const classes = useStyles();
+
   return (
     <FormControl variant="filled">
-      <InputLabel>{label}</InputLabel>
-      <Select value={selectedOptionId} onChange={onChange} disableUnderline>
+      {label && <InputLabel>{label}</InputLabel>}
+      <Select
+        inputProps={{
+          className: clsx({ [classes.noLabelInput]: !label }),
+        }}
+        value={selectedOptionId}
+        onChange={onChange}
+        disableUnderline
+      >
         {options.map(({ id, name }) => (
           <MenuItem key={id} value={id}>
             {name}
