f0fa20924130e0e2dc116454910c20e70bb83bb7
tdarneix <tdarneix@centreon.com>
2020-02-25 16:36:32 +0100

    Fix snackbar stories

--- src/Snackbar/index.stories.tsx
+++ src/Snackbar/index.stories.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 import Snackbar from '.';
 
-export default { title: 'Snackbar/Error' };
+export default { title: 'Snackbar' };
 
 export const errorSnackbar = (): JSX.Element => (
   <Snackbar open message="Something unexpected happened..." isError />
 );
 
 export const successSnackbar = (): JSX.Element => (
-  <Snackbar open message="Something unexpected happened..." />
+  <Snackbar open message="Something successful happened..." />
 );
