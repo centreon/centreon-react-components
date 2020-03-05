c6ea0e5e060d8b496fd80533d9d4b05b29c4f196
crubin <rubin.coralie@gmail.com>
2020-03-05 17:10:21 +0100

    issue EsLint done

--- src/TopFilters/index.js
+++ src/TopFilters/index.js
@@ -4,18 +4,18 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react/prefer-stateless-function */
 
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import clsx from 'clsx';
+import {Switch, FormControlLabel} from '@material-ui/core';
 import styles from '../global-sass-files/_grid.scss';
 import filterStyles from './top-filters.scss';
 import Wrapper from '../Wrapper';
 import SearchLive from '../Search/SearchLive';
-import {Switch, FormControlLabel} from '@material-ui/core';
 import Button from '../Button';
 
 class TopFilters extends Component {
   render() {
-    const { fullText, switchers, onChange } = this.props;
+    const { fullText, switchs, onChange } = this.props;
 
     return (
       <div className={styles['container-gray']}>
@@ -40,17 +40,17 @@ class TopFilters extends Component {
               ) : null}
 
               <div className={clsx(styles.container__row)}>
-                {switchers
-                  ? switchers.map((switcherColumn, index) => (
+                {switchs
+                  ? switchs.map((switchColumn, index) => (
                     <div
-                      key={`switcherSubColumn${index}`}
+                      key={`switchSubColumn${index}`}
                       className={filterStyles['switch-wrapper']}
                     >
-                      {switcherColumn.map(
+                      {switchColumn.map(
                           (
                             {
-                              switcherTitle,
-                              switcherStatus,
+                              switchTitle,
+                              switchStatus,
                               button,
                               label,
                               buttonType,
@@ -63,16 +63,18 @@ class TopFilters extends Component {
                           ) =>
                             !button ? (
                               <FormControlLabel
-                                control={<Switch color="primary" onChange={(e) => onChange(e.target.value, filterKey)} />}
-                                label={<>
-                                  {switcherTitle && <div><b>{switcherTitle}</b></div>}
-                                {switcherStatus && <div>{switcherStatus}</div>}
-                                  </>}
+                                control={<Switch checked={value} color="primary" onChange={(e) => onChange(e.target.checked, filterKey)} />}
+                                label={(
+                                  <>
+                                    {switchTitle && <div><b>{switchTitle}</b></div>}
+                                    {switchStatus && <div>{switchStatus}</div>}
+                                  </>
+)}
                                 labelPlacement="top"
                               />
                             ) : (
                               <div
-                                key={`switcher${index}${i}`}
+                                key={`switch${index}${i}`}
                                 className={clsx(
                                   styles['container__col-sm-6'],
                                   styles['container__col-xs-4'],
@@ -82,7 +84,7 @@ class TopFilters extends Component {
                                 )}
                               >
                                 <Button
-                                  key={`switcherButton${index}${i}`}
+                                  key={`switchButton${index}${i}`}
                                   label={label}
                                   buttonType={buttonType}
                                   color={color}
