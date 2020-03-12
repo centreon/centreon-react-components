6f495e9dc33d262ee37e122ca9807d8fe45fbcf1
Thebarda <tomdar87@outlook.com>
2020-03-12 18:10:23 +0100

    Fix display snackbar when closing it

--- src/Snackbar/index.tsx
+++ src/Snackbar/index.tsx
@@ -1,14 +1,8 @@
 import React from 'react';
 
-import {
-  Snackbar,
-  SnackbarContent,
-  IconButton,
-  makeStyles,
-  Theme,
-} from '@material-ui/core';
-import { Error, CheckCircle, Warning, Info, Close } from '@material-ui/icons';
-import { CreateCSSProperties } from '@material-ui/styles';
+import { Snackbar as MuiSnackbar, IconButton, makeStyles } from '@material-ui/core';
+import { Close } from '@material-ui/icons';
+import { Alert } from '@material-ui/lab';
 
 import Severity from './Severity';
 
@@ -16,20 +10,15 @@ interface PropsStyle {
   getColor: (theme) => string;
 }
 
-const useStyles = makeStyles<Theme, PropsStyle>((theme) => ({
-  iconColor: ({ getColor }: PropsStyle): CreateCSSProperties<PropsStyle> => ({
-    backgroundColor: getColor(theme),
-    marginRight: theme.spacing(1),
-  }),
-  icon: {
+const useStyles = makeStyles<PropsStyle>({
+  closeIcon: {
     fontSize: 20,
     opacity: 0.9,
   },
-  message: {
-    display: 'flex',
-    alignItems: 'center',
+  alertIcon: {
+    paddingTop: '10px',
   },
-}));
+});
 
 interface Props {
   message: string;
@@ -38,64 +27,38 @@ interface Props {
   severity?: Severity;
 }
 
-const snackbarIcons = {
-  error: {
-    Icon: (props): JSX.Element => <Error {...props} />,
-    getColor: (theme): string => theme.palette.error.dark,
-  },
-  warning: {
-    Icon: (props): JSX.Element => <Warning {...props} />,
-    getColor: (theme): string => theme.palette.warning.dark,
-  },
-  success: {
-    Icon: (props): JSX.Element => <CheckCircle {...props} />,
-    getColor: (theme): string => theme.palette.success.dark,
-  },
-  info: {
-    Icon: (props): JSX.Element => <Info {...props} />,
-    getColor: (theme): string => theme.palette.info.dark,
-  },
-};
-
-const ErrorSnackbar = ({
+const Snackbar = ({
   message,
   open,
   onClose,
   severity = Severity.success,
 }: Props): JSX.Element => {
-  const { Icon, getColor } = snackbarIcons[severity];
-  const classes = useStyles({ getColor });
-
-  const classNames = `${classes.icon} ${classes.iconColor}`;
-
-  const Message = (
-    <span className={classes.message}>
-      <Icon className={classNames} />
-      {message}
-    </span>
-  );
+  const classes = useStyles();
 
   return (
-    <Snackbar
+    <MuiSnackbar
       anchorOrigin={{
         vertical: 'bottom',
         horizontal: 'center',
       }}
       open={open}
       autoHideDuration={6000}
-      onExited={onClose}
+      onClose={onClose}
     >
-      <SnackbarContent
-        className={classes.iconColor}
-        message={Message}
+      <Alert
+        variant="filled"
+        severity={severity}
         action={[
           <IconButton key="close" color="inherit" onClick={onClose}>
-            <Close className={classes.icon} />
+            <Close className={classes.closeIcon} />
           </IconButton>,
         ]}
-      />
+        classes={{ icon: classes.alertIcon }}
+      >
+        {message}
+      </Alert>
     </Snackbar>
   );
 };
 
-export default ErrorSnackbar;
+export default Snackbar;
