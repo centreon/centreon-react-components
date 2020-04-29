b804844f42529007e484522bdb685906e7c0ae75
Thebarda <tomdar87@outlook.com>
2020-04-29 11:02:07 +0200

    Fix custom labels display on action bar

--- src/Wizard/ActionBar.test.jsx
+++ src/Wizard/ActionBar.test.jsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import * as React from 'react';
 import { render, fireEvent } from '@testing-library/react';
 import ActionBar from './ActionBar';
+
+const mockFn = jest.fn();
 
 describe('ActionBar', () => {
   it('cancels', () => {
@@ -19,5 +21,41 @@ describe('ActionBar', () => {
     const { getByText } = render(<ActionBar disabledNext />);
 
     expect(getByText('Finish').parentNode).toHaveAttribute('disabled');
+  });
+
+  it('displays custom previous, next button label when wizar is on second step and not on last step', () => {
+    const { getByText } = render(
+      <ActionBar
+        isLastPage={false}
+        page={1}
+        labelPrevious="Custom previous"
+        labelNext="Custom next"
+      />,
+    );
+
+    expect(getByText('Custom previous')).toBeInTheDocument();
+    expect(getByText('Custom next')).toBeInTheDocument();
+  });
+
+  it('displays custom previous, finish button label when wizard is on last step', () => {
+    const { getByText } = render(
+      <ActionBar
+        isLastPage
+        page={1}
+        labelPrevious="Custom previous"
+        labelFinish="Custom finish"
+      />,
+    );
+
+    expect(getByText('Custom previous')).toBeInTheDocument();
+    expect(getByText('Custom finish')).toBeInTheDocument();
+  });
+
+  it('displays custom cancel button label when wizard can be canceled', () => {
+    const { getByText } = render(
+      <ActionBar onCancel={mockFn} labelCancel="Custom cancel" />,
+    );
+
+    expect(getByText('Custom cancel')).toBeInTheDocument();
   });
 });
