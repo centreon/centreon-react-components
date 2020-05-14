0c551a494bd2a36f993fec66a8b53105c0676cef
Thebarda <tomdar87@outlook.com>
2020-05-14 11:47:05 +0200

    Export post, put and delete axios method

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,4 +3,31 @@ import axios from 'axios';
 const getData = <TData>(cancelToken) => (endpoint): Promise<TData> =>
   axios.get(endpoint, { cancelToken }).then(({ data }) => data);
 
-export { getData };
+const postData = <TData>(cancelToken) => ({
+  endpoint,
+  datas,
+}): Promise<TData> =>
+  axios
+    .post(endpoint, datas, {
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      cancelToken,
+    })
+    .then(({ data }) => data);
+
+const putData = <TData>(cancelToken) => ({ endpoint, datas }): Promise<TData> =>
+  axios
+    .put(endpoint, datas, {
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      cancelToken,
+    })
+    .then(({ data }) => data);
+
+const deleteData = <TData>(cancelToken) => (endpoint): Promise<TData> =>
+  axios
+    .delete(endpoint, {
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      cancelToken,
+    })
+    .then(({ data }) => data);
+
+export { getData, postData, putData, deleteData };
