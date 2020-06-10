69a3a7fce1f8d48e5dce1b00dcaa12a164de1578
Thebarda <tomdar87@outlook.com>
2020-06-10 15:20:34 +0200

    Add stories

---
+++ src/ListingPage/index.stories.tsx
@@ -0,0 +1,204 @@
+import * as React from 'react';
+
+import { Typography, makeStyles, Paper } from '@material-ui/core';
+import { grey } from '@material-ui/core/colors';
+
+import ListingPage from '.';
+import Listing from '../Listing';
+import ColumnTypes from '../Listing/ColumnTypes';
+import { SearchField } from '..';
+import TextField from '../InputField/Text';
+import AutocompleteField from '../InputField/Select/Autocomplete';
+
+export default { title: 'Listing Page' };
+
+const noOp = (): void => undefined;
+
+const useStyles = makeStyles((theme) => ({
+  filtersSummary: {
+    display: 'grid',
+    gridTemplateColumns: 'auto 1fr',
+    gridGap: theme.spacing(2),
+    alignItems: 'center',
+  },
+  autoComplete: {
+    width: 250,
+  },
+  detailsPanel: {
+    display: 'grid',
+    gridTemplateRows: 'auto 1fr',
+    gridTemplateColumns: '94%',
+    justifyContent: 'center',
+    marginTop: theme.spacing(2),
+    gridGap: theme.spacing(4),
+  },
+  detailsContent: {
+    display: 'grid',
+    gridTemplateAreas: `
+      'title title'
+      'description comment'
+    `,
+    gridTemplateRows: '50px 100px',
+    gridGap: theme.spacing(2),
+  },
+  title: {
+    gridArea: 'title',
+    alignItems: 'center',
+    textAlign: 'center',
+  },
+  description: {
+    gridArea: 'description',
+  },
+  comment: {
+    gridArea: 'comment',
+  },
+}));
+
+const configuration = [
+  {
+    id: 'name',
+    label: 'Name',
+    type: ColumnTypes.string,
+    getFormattedString: ({ name }): string => name,
+  },
+  { id: 'active', label: 'Active', type: ColumnTypes.toggler },
+  {
+    id: 'description',
+    label: 'Description',
+    type: ColumnTypes.string,
+    getFormattedString: ({ description }): string => description,
+  },
+  {
+    id: 'alias',
+    label: 'Alias',
+    type: ColumnTypes.string,
+    getFormattedString: ({ alias }): string => alias,
+  },
+];
+
+const fifteenElements = new Array(15).fill(0);
+
+const elements = [...fifteenElements].map((_, index) => ({
+  id: index,
+  name: `E${index}`,
+  description: `Entity ${index}`,
+  alias: `Alias ${index}`,
+  active: index % 2 === 0,
+}));
+
+const rowColorConditions = [
+  {
+    name: 'inactive',
+    condition: ({ active }): boolean => !active,
+    color: grey[500],
+  },
+];
+
+const options = [
+  { id: 0, name: 'First Entity' },
+  { id: 1, name: 'Second Entity' },
+  { id: 2, name: 'Third Entity' },
+];
+
+const listing = (
+  <Listing
+    columnConfiguration={configuration}
+    onDelete={noOp}
+    onSort={noOp}
+    onDuplicate={noOp}
+    onPaginate={noOp}
+    onPaginationLimitChanged={noOp}
+    limit={elements.length}
+    currentPage={0}
+    totalRows={elements.length}
+    tableData={elements}
+    rowColorConditions={rowColorConditions}
+  />
+);
+
+const FiltersSummary = (): JSX.Element => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.filtersSummary}>
+      <Typography>Filters</Typography>
+      <SearchField />
+    </div>
+  );
+};
+
+const FiltersDetails = (): JSX.Element => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.filtersSummary}>
+      <TextField placeholder="Text" />
+      <AutocompleteField
+        className={classes.autoComplete}
+        options={options}
+        label="Autocomplete"
+        placeholder="Type here..."
+        value={options[0]}
+      />
+      <AutocompleteField
+        className={classes.autoComplete}
+        options={options}
+        label="Other Autocomplete"
+        placeholder="Type here..."
+        value={options[1]}
+      />
+      <TextField placeholder="Other Text" />
+    </div>
+  );
+};
+
+const DetailsPanel = (): JSX.Element => {
+  const classes = useStyles();
+  return (
+    <div className={classes.detailsPanel}>
+      <Typography variant="h5" align="center">
+        Details Panel
+      </Typography>
+      <div className={classes.detailsContent}>
+        <Paper className={classes.title}>
+          <Typography>My Title</Typography>
+        </Paper>
+        <Paper className={classes.description}>
+          <Typography>Description</Typography>
+        </Paper>
+        <Paper className={classes.comment}>
+          <Typography>Comment</Typography>
+        </Paper>
+      </div>
+    </div>
+  );
+};
+
+export const normal = (): JSX.Element => (
+  <ListingPage
+    openSlidePanel={false}
+    listing={listing}
+    filtersExpandable={false}
+    filters={<FiltersSummary />}
+  />
+);
+
+export const normalWithOpenedPanel = (): JSX.Element => (
+  <ListingPage
+    openSlidePanel
+    listing={listing}
+    filtersExpandable={false}
+    filters={<FiltersSummary />}
+    slidePanel={<DetailsPanel />}
+  />
+);
+
+export const normalWithFiltersDetails = (): JSX.Element => (
+  <ListingPage
+    openSlidePanel={false}
+    listing={listing}
+    filtersExpandable
+    filters={<FiltersSummary />}
+    expandableFilters={<FiltersDetails />}
+  />
+);
