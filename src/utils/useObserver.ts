c5b1c055a4f3290cbc5bf2b1b54d4b3d67221b24
Thebarda <tomdar87@outlook.com>
2020-07-03 18:18:21 +0200

    Add intersection observer hook

---
+++ src/utils/useObserver.ts
@@ -0,0 +1,41 @@
+import * as React from 'react';
+
+interface HookParam {
+  maxPage: number;
+  page: number;
+  loading: boolean;
+  action: () => void;
+}
+
+const useObserver = ({
+  maxPage,
+  page,
+  loading,
+  action,
+}: HookParam): ((node) => void) => {
+  const observer = React.useRef<IntersectionObserver | null>(null);
+  const lastElementRef = React.useCallback(
+    (node) => {
+      if (observer.current) {
+        observer.current.disconnect();
+      }
+      if (loading) {
+        observer.current = null;
+      } else {
+        observer.current = new IntersectionObserver((entries) => {
+          if (entries[0].isIntersecting && page < maxPage) {
+            action();
+          }
+        });
+      }
+      if (node && observer.current) {
+        observer.current.observe(node);
+      }
+    },
+    [maxPage, page, loading],
+  );
+
+  return lastElementRef;
+};
+
+export default useObserver;
