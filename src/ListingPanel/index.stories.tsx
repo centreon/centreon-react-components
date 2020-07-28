a210268d78c1bc7027535d8db3985b664ae1199c
Thebarda <tomdar87@outlook.com>
2020-07-28 11:00:43 +0200

    Remove loading state from normal story

--- src/ListingPanel/index.stories.tsx
+++ src/ListingPanel/index.stories.tsx
@@ -10,7 +10,6 @@ export const normal = (): JSX.Element => (
   <ListingPanel
     active
     Header={<Typography>Header</Typography>}
-    loading
     Sections={[
       {
         expandable: true,
