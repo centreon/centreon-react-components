9e327bf3c9e2c79aa8c82a0b95851ab3303e5570
Tom Darneix <tomdar87@outlook.com>
2020-08-10 09:51:04 +0200

    Remove duplicate import

--- src/InputField/Select/Autocomplete/Connected/index.tsx
+++ src/InputField/Select/Autocomplete/Connected/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-import { concat, last, equals, path, append, last } from 'ramda';
+import { concat, equals, path, append, last } from 'ramda';
 import { useDebouncedCallback } from 'use-debounce';
 
 import {
