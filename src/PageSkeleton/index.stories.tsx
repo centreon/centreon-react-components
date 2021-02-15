5f51089d52158c09a2411e170cb617203f607c54
Thebarda <tomdar87@outlook.com>
2021-02-15 14:21:25 +0100

    Implement a menu loader skeleton

--- src/PageSkeleton/index.stories.tsx
+++ src/PageSkeleton/index.stories.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 
+import MenuLoader from './MenuSkeleton';
+
 import PageSkeleton from '.';
 
 export default { title: 'Page Skeleton' };
@@ -8,4 +10,16 @@ export const normal = (): JSX.Element => <PageSkeleton animate={false} />;
 
 export const normalWidthHeaderAndNavigation = (): JSX.Element => (
   <PageSkeleton displayHeaderAndNavigation animate={false} />
+);
+
+export const menuLoader = (): JSX.Element => (
+  <div style={{ backgroundColor: '#444455' }}>
+    <MenuLoader animate={false} />
+  </div>
+);
+
+export const menuLoaderWithCustomWidth = (): JSX.Element => (
+  <div style={{ backgroundColor: '#444455' }}>
+    <MenuLoader width={40} animate={false} />
+  </div>
 );
