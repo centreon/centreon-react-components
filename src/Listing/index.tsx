e8aee84f3acd0c12d9d43b3b3634931bd7fdc774
Bruno d'Auria <bdauria@centreon.com>
2021-02-23 18:06:25 +0100

    Update src/Listing/index.tsx
    Co-authored-by: Tom Darneix <tomdar87@outlook.com>

--- src/Listing/index.tsx
+++ src/Listing/index.tsx
@@ -218,7 +218,7 @@ const Listing = ({
   };
 
   const getGridTemplateColumn = (): string => {
-    const checkbox = checkable ? 'auto ' : '';
+    const checkbox = checkable ? 'min-content' : '';
 
     const columns = columnConfiguration
       .map(({ width }) => {
